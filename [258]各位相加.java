bb60fbb62a6f96eb77701dcd4e25751b2a9ba758
Ye <965558902@qq.com>
2020-11-05 10:19:42 +0800

    Add files via upload

---
+++ [258]各位相加.java
@@ -0,0 +1,32 @@
+//给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。 
+//
+// 示例: 
+//
+// 输入: 38
+//输出: 2 
+//解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+// 
+//
+// 进阶: 
+//你可以不使用循环或者递归，且在 O(1) 时间复杂度内解决这个问题吗？ 
+// Related Topics 数学 
+// 👍 294 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int addDigits(int num) {
+        //int count = 0;
+        if (num == 0) return 0;
+        while(num / 10 != 0){
+            int count = 0;
+            while(num != 0){
+                count = count + num % 10;
+                num = num / 10;
+            }
+            num = count;
+        }
+        return num;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
