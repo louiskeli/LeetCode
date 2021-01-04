fdc5b065275c06b47ae9f7c35ccf30cf4e2985ec
Ye <965558902@qq.com>
2021-01-04 14:52:39 +0800

    Add files via upload

---
+++ [453]最小操作次数使数组元素相等.java
@@ -0,0 +1,31 @@
+//给定一个长度为 n 的 非空 整数数组，每次操作将会使 n - 1 个元素增加 1。找出让数组所有元素相等的最小操作次数。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：
+//[1,2,3]
+//输出：
+//3
+//解释：
+//只需要3次操作（注意每次操作会增加两个元素的值）：
+//[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+// 
+// Related Topics 数学 
+// 👍 179 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = nums.length - 1; i > 0 ; i--){
+            count += nums[i] - nums[0];
+        }
+        return count;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
