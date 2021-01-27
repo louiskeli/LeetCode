e30e4e320b40df8983bb22a1734ce1916228619c
Ye <965558902@qq.com>
2021-01-27 11:23:06 +0800

    Add files via upload

---
+++ [6]Z 字形变换.java
@@ -0,0 +1,86 @@
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。 
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+//
+// 
+//P   A   H   N
+//A P L S I I G
+//Y   I   R 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// 
+//string convert(string s, int numRows); 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+// 
+//示例 2：
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "A", numRows = 1
+//输出："A"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由英文字母（小写和大写）、',' 和 '.' 组成 
+// 1 <= numRows <= 1000 
+// 
+// Related Topics 字符串 
+// 👍 988 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String convert(String s, int numRows) {
+        int n = s.length();
+        if (numRows == 1) return s;
+
+        //使用类型为StringBuffer的列表来存储每行字符串
+        List<StringBuilder> rows = new ArrayList<>();
+        //行数由numRows和字符串s长度的最小值决定，给列表中的每行初始化
+        for (int i = 0; i < Math.min(numRows,n); i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            //只有当我们向上移动到最上面的行或向下移动到最下面的行时，当前方向才会发生改变。
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        //将列表中存储的每行字符串加到最后结果中
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
